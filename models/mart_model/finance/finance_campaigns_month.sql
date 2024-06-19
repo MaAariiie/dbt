fe04abfc52e3c4a9d09fd326641789a9232028d9
Marie DELVART <marie3.lewagon@gmail.com>
2024-06-19 13:04:02 +0000

    exo 5

--- models/mart_model/finance/finance_campaigns_month.sql
+++ models/mart_model/finance/finance_campaigns_month.sql
@@ -1,3 +1,10 @@
-SELECT *,
-DATE_FORMAT(date_date,‘YYYY-MM’) AS date_month
+SELECT
+DATE_TRUNC(date_date,month) AS date_month,
+SUM(ads_margin) AS ads_margin,
+SUM(average_basket) AS average_basket,
+SUM(operational_margin) AS operational_margin,
+SUM(ads_cost) AS ads_cost,
+SUM(ads_impression) AS ads_impression
+
 FROM {{ref("finance_campaigns_day")}}
+GROUP BY date_month
