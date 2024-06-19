21f4366fa7b5aa6e07847b3e2a2d6c50b174c8f3
Marie DELVART <marie3.lewagon@gmail.com>
2024-06-19 12:24:46 +0000

    ads margin

--- models/mart_model/finance/finance_campaigns_day.sql
+++ models/mart_model/finance/finance_campaigns_day.sql
@@ -1,6 +1,9 @@
-SELECT *
-FROM {{ref("int_campaign_day")}}
+WITH new_table AS (SELECT *
+FROM {{ref("int_campaign_day")}} 
 FULL OUTER JOIN
-{{ref("finance_days")}}
-ON date_date
-ORDER BY date_date
+{{ref("finance_days")}} 
+USING (date_date))
+
+SELECT *,
+operational_margin-ads_cost AS ads_margin
+FROM new_table
